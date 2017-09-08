515e48dc4ffeaa87006870fc7c7155a471f2a750
Jeff Ridenbaugh <zeff84@gmail.com>
2017-09-08 16:50:26 -0400

    updated settings for new laptop

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,26 @@
-﻿
-# Load posh-git example profile
-. 'C:\Users\jridenbaugh\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+﻿Set-Location C:\workspace
 
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+Import-Module posh-git
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    
+    Write-Host($env:username) -nonewline -foregroundcolor Magenta
+    Write-Host (" ") -nonewline -foregroundcolor Green
+    Write-Host($pwd.ProviderPath) -nonewline -foregroundcolor White
+
+    Write-VcsStatus
+    
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    
+    Write-Host ("") -foregroundcolor Green
+    Write-Host ("λ") -nonewline -foregroundcolor Red
+    return " "
+}
+
+Pop-Location
+
+Start-SshAgent -Quiet
